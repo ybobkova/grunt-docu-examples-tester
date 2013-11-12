8954d5b49f6d02d658938c8d8906a5d4d0d1ba87
Yulia Bobkova <y.bobkova@ps-webforge.com>
2013-11-12 23:00:34 +0100

    file for testing push()

---
+++ test/files/pushExampleTest.js
@@ -0,0 +1,28 @@
+/*jshint node: true */
+/*global describe, it, before, beforeEach, after, afterEach */
+"use strict";
+var chai = require("chai");
+var mocha = require("mocha");
+var requirejs = require("requirejs");
+var expect = chai.expect, assert = chai.assert;
+var path = require("path");
+var _ = require("lodash");
+var lib = path.resolve(__dirname + "/../../../lib");
+var cojokoConfig = _.merge(require(lib + "\\config.js"), { baseUrl: lib });
+var cojokoLoader = requirejs.config(cojokoConfig);
+describe("first", function () {
+  this.timeout(500);
+  it("has value as a property", function (done) {
+    setTimeout(done, 300);
+    cojokoLoader(["Cojoko"], function (Cojoko) {
+      /* beginning of the example */
+      Cojoko.Class("ACME.Exchange.Price", {
+        properties: {
+          value: {is: "g", required: true, isPrivate: true}
+        }
+      });
+      /* end of the example */
+      assert.equal(true, true);
+    });
+  });
+});
